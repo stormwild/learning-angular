fd90b9ee80017a8b0d7582a0233ae583c1949b31
Alexander R. Torrijos <artorrijos@gmail.com>
2017-09-21 10:26:16 +0000

    Added image and conditional class

--- src/app/car-parts.component.ts
+++ src/app/car-parts.component.ts
@@ -5,13 +5,17 @@ import { CARPARTS } from './mocks';
 @Component({
     selector: 'car-parts',
     templateUrl: './car-parts.component.html',
+    styleUrls: ['./car-parts.component.css'],
     providers: [CarPart]
 })
 export class CarPartsComponent {
+    imageBasePath: string;
+    
     carParts: CarPart[];
     
     ngOnInit() {
         this.carParts = CARPARTS;
+        this.imageBasePath = '/assets/img/';
     }
     
     totalCarParts() {
--- src/app/mocks.ts
+++ src/app/mocks.ts
@@ -5,17 +5,23 @@ export const CARPARTS: CarPart[] = [{
         "name": "Super Tires",
         "description": "These are the very best tires",
         "inStock": 5,
-        "price": 4.99
+        "price": 4.99,
+        "image": 'daytona_thunderdome.jpg',
+        "featured": true
     }, {
         "id": 2,
         "name": "Reinforced Shocks",
         "description": "Shocks made from kryptonite",
         "inStock": 4,
-        "price": 9.99
+        "price": 9.99,
+        "image": 'new_york_city_skyline.jpg',
+        "featured": false
     }, {
         "id": 2,
         "name": "Padded Seats",
         "description": "Super soft seats for a smooth ride",
         "inStock": 0,
-        "price": 24.99
+        "price": 24.99,
+        "image": 'san_francisco_ruins.jpg',
+        "featured": true
     }];
