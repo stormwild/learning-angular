48a67183e7419ef3af525ab5fd1fe8c6cede14fc
Alexander R. Torrijos <artorrijos@gmail.com>
2017-09-21 09:11:05 +0000

    Refactored data to separate file

--- src/app/car-parts.component.ts
+++ src/app/car-parts.component.ts
@@ -1,30 +1,19 @@
 import { Component } from '@angular/core';
+import { CarPart } from './car-part';
+import { CARPARTS } from './mocks';
 
 @Component({
     selector: 'car-parts',
     templateUrl: './car-parts.component.html',
+    providers: [CarPart]
 })
 export class CarPartsComponent {
-    carParts = [{
-        "id": 1,
-        "name": "Super Tires",
-        "description": "These are the very best tires",
-        "inStock": 5,
-        "price": 4.99
-    }, {
-        "id": 2,
-        "name": "Reinforced Shocks",
-        "description": "Shocks made from kryptonite",
-        "inStock": 4,
-        "price": 9.99
-    }, {
-        "id": 2,
-        "name": "Padded Seats",
-        "description": "Super soft seats for a smooth ride",
-        "inStock": 0,
-        "price": 24.99
-    }];
-
+    carParts: CarPart[];
+    
+    ngOnInit() {
+        this.carParts = CARPARTS;
+    }
+    
     totalCarParts() {
         return this.carParts.reduce((prev, current) => prev + current.inStock, 0)
     }
---
+++ src/app/mocks.ts
@@ -0,0 +1,21 @@
+import { CarPart } from './car-part'; 
+
+export const CARPARTS: CarPart[] = [{
+        "id": 1,
+        "name": "Super Tires",
+        "description": "These are the very best tires",
+        "inStock": 5,
+        "price": 4.99
+    }, {
+        "id": 2,
+        "name": "Reinforced Shocks",
+        "description": "Shocks made from kryptonite",
+        "inStock": 4,
+        "price": 9.99
+    }, {
+        "id": 2,
+        "name": "Padded Seats",
+        "description": "Super soft seats for a smooth ride",
+        "inStock": 0,
+        "price": 24.99
+    }];
