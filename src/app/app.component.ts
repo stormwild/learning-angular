1a52d5292d373105a70516b870869933a680c871
Alexander R. Torrijos <artorrijos@gmail.com>
2017-09-21 08:09:18 +0000

    Using arrays and calling component method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,26 @@ export class AppComponent {
     "id": 1,
     "name": "Super Tires",
     "description": "These are the very best tires",
-    "inStock": 5
+    "inStock": 5,
+    "price": 4.99
   }, {
     "id": 2,
     "name": "Reinforced Shocks",
     "description": "Shocks made from kryptonite",
-    "inStock": 4
+    "inStock": 4,
+    "price": 9.99
   }, {
     "id": 2,
     "name": "Padded Seats",
     "description": "Super soft seats for a smooth ride",
-    "inStock": 0
+    "inStock": 0,
+    "price": 24.99
   }];
+  totalCarParts() {
+    let sum = 0;
+    for(let carPart of this.carParts) {
+      sum += carPart.inStock;
+    }
+    return sum;
+  }
 }
